0ea8be4c92422f0240145d8ef439f5cf81a50197
sharat422 <sharatreddy46@gmail.com>
2026-03-29 14:45:00 -0400

    updated code

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2779508005725169"
           crossOrigin="anonymous"
-          strategy="beforeInteractive"
+          strategy="afterInteractive"
         />
 
         {/* NAVBAR */}
